11bc89ac10e9990bd76613c29d25f0e1c8bfe3a1
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-10-01 13:04:56 -0300

    Update fonts.js

---
+++ src/utils/fonts.js
@@ -0,0 +1,5 @@
+import Poppins_400Regular from "../../assets/fonts/Poppins-Regular.ttf";
+import Poppins_500Medium from "../../assets/fonts/Poppins-Medium.ttf";
+import Poppins_600SemiBold from "../../assets/fonts/Poppins-SemiBold.ttf";
+
+export { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold };
