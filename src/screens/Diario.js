400506d4cb3d430dd9b229dbf2cdd6eb34e583b5
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-10-04 15:56:21 -0300

    Update Diario.js

--- src/screens/Diario.js
+++ src/screens/Diario.js
@@ -1,15 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import React from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Diario() {
+export default function Diario({navigation}) {
   return (
     <View style={styles.container}>
       <View style={styles.viewMenu}>
+        <Pressable onPress={() => {navigation.navigate("Home")}}>
+
         <MaterialCommunityIcons
           name="chevron-left"
-          size={30}
+          size={35}
         />
+        </Pressable>
         <View>
           <Text>DIario</Text>
           <Text>DIario</Text>
