891e686f1fee3667683b943034765aa55a712b35
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-10-05 13:20:52 -0300

    Update Rotina.js

--- src/screens/Rotina.js
+++ src/screens/Rotina.js
@@ -1,10 +1,24 @@
-import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { globalStyles } from "../utils/globalStyles";
 
-export default function Rotina() {
+export default function Rotina({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Rotina</Text>
+      <View style={styles.viewMenu}>
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        >
+          <MaterialCommunityIcons name="chevron-left" size={35} />
+        </Pressable>
+
+        <View style={styles.viewTexto}>
+          <Text style={[globalStyles.mediumText]}>Rotinas</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,7 +27,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  viewMenu: {
+    marginVertical: 30,
+    padding: 4,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "73%",
+    backgroundColor: "red",
   },
 });
