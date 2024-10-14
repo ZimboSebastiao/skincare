24c4a8b5fcbd96e04cc367dcf2ec512ee17a229b
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-10-14 15:35:32 -0300

    Update Manha.js

--- src/screens/Manha.js
+++ src/screens/Manha.js
@@ -89,6 +89,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "space-between", 
+    padding: 10
   },
   checkboxContainer: {
     flexDirection: "row",
