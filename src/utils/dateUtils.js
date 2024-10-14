b092acb5ba9dc3dbcb30e3d19cd76534dc8373f0
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-10-14 13:47:17 -0300

    Update dateUtils.js

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -2,6 +2,7 @@ export function getCurrentDate() {
   const today = new Date();
   const day = today.getDate();
   const month = today.toLocaleString("pt-BR", { month: "long" });
+  const year = today.getFullYear();
 
   return `${day} de ${month.charAt(0).toUpperCase() + month.slice(1)}`;
 }
