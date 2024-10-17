1030cb4c78ce643104b03be02481844e39f2e28c
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-10-17 03:41:57 -0300

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -17,8 +17,8 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Svg, { Path } from "react-native-svg";
 import { Badge } from "react-native-paper";
 import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
-import { Bell } from 'lucide-react-native';
-import Swiper from 'react-native-swiper';
+import { Bell } from "lucide-react-native";
+import Swiper from "react-native-swiper";
 
 export default function Home({ navigation }) {
   const { selectedImage } = useContext(ImageContext);
@@ -38,24 +38,23 @@ export default function Home({ navigation }) {
 
   return (
     <>
-
       <View style={styles.viewMenu}>
         <View style={styles.viewNotificacao}>
-        <Text style={[styles.textoMenu, globalStyles.semiBoldText]}>
-          Hoje é, {currentDate}
-        </Text>
-        <Pressable style={styles.bellContainer}>
-          <Bell color="#121212" size={30} />
-          <Badge
-            style={styles.badge}
-            size={20}
-          >
-            3
-          </Badge>
-        </Pressable>
+          <Text style={[styles.textoMenu, globalStyles.semiBoldText]}>
+            Hoje é, {currentDate}
+          </Text>
+          <Pressable style={styles.bellContainer}>
+            <Bell color="#121212" size={30} />
+            <Badge style={styles.badge} size={20}>
+              3
+            </Badge>
+          </Pressable>
         </View>
         <View style={styles.viewImagem}>
-          <CustomAvatar imageUri={selectedImage} size={70} />
+          {/* <CustomAvatar imageUri={selectedImage} size={70} /> */}
+          <View style={styles.avatarContainer}>
+            <CustomAvatar imageUri={selectedImage} size={70} />
+          </View>
           <View>
             <Text style={[globalStyles.semiBoldText, styles.textoImagem]}>
               {greeting},
@@ -66,165 +65,165 @@ export default function Home({ navigation }) {
           </View>
         </View>
       </View>
-    <ScrollView style={styles.container}>
-
-      <View style={styles.viewPaginas}>
-        <View style={styles.viewPagina}>
-          <Pressable
-            style={styles.buttonWrapper}
-            onPress={() => {
-              navigation.navigate("Scan");
-            }}
-          >
-            <Svg
-              width="90"
-              height="110"
-              viewBox="0 0 109 123"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+      <ScrollView style={styles.container}>
+        <View style={styles.viewPaginas}>
+          <View style={styles.viewPagina}>
+            <Pressable
+              style={styles.buttonWrapper}
+              onPress={() => {
+                navigation.navigate("Scan");
+              }}
             >
-              <Path
-                d="M16.8846 95.4338C-1.49599 85.665 -5.25823 60.9379 9.38521 46.1445L34.8438 20.4253C36.8506 18.3979 38.3812 15.9495 39.3247 13.2574V13.2574C44.2839 -0.892831 62.8106 -4.17536 72.3295 7.4097L84.5358 22.2654C88.9527 27.641 91.3672 34.3828 91.3672 41.3402V42.9851C91.3672 48.2103 92.8969 53.3212 95.7676 57.6872L105.534 72.541C114.546 86.2478 103.011 104.14 86.8065 101.589V101.589C78.8025 100.329 70.8861 104.312 67.1277 111.49L66.8739 111.975C59.7559 125.57 40.5021 126.132 32.6028 112.976L28.023 105.349C25.7179 101.51 22.4108 98.3708 18.4567 96.2693L16.8846 95.4338Z"
-                fill="#ff80c3"
-              />
-            </Svg>
+              <Svg
+                width="90"
+                height="110"
+                viewBox="0 0 109 123"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <Path
+                  d="M16.8846 95.4338C-1.49599 85.665 -5.25823 60.9379 9.38521 46.1445L34.8438 20.4253C36.8506 18.3979 38.3812 15.9495 39.3247 13.2574V13.2574C44.2839 -0.892831 62.8106 -4.17536 72.3295 7.4097L84.5358 22.2654C88.9527 27.641 91.3672 34.3828 91.3672 41.3402V42.9851C91.3672 48.2103 92.8969 53.3212 95.7676 57.6872L105.534 72.541C114.546 86.2478 103.011 104.14 86.8065 101.589V101.589C78.8025 100.329 70.8861 104.312 67.1277 111.49L66.8739 111.975C59.7559 125.57 40.5021 126.132 32.6028 112.976L28.023 105.349C25.7179 101.51 22.4108 98.3708 18.4567 96.2693L16.8846 95.4338Z"
+                  fill="#ff80c3"
+                />
+              </Svg>
 
-            <View style={styles.buttonContent}>
-              <Image
-                source={require("../../assets/images/generator.png")}
-                style={styles.produtoImage}
-              />
-            </View>
-            <Text style={[styles.buttonText, globalStyles.text]}>Análise</Text>
-          </Pressable>
-        </View>
-        <View style={styles.viewPagina}>
-          <Pressable
-            style={styles.buttonWrapper}
-            onPress={() => {
-              navigation.navigate("Relatorio");
-            }}
-          >
-            <Svg
-              width="90"
-              height="110"
-              viewBox="0 0 109 123"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+              <View style={styles.buttonContent}>
+                <Image
+                  source={require("../../assets/images/generator.png")}
+                  style={styles.produtoImage}
+                />
+              </View>
+              <Text style={[styles.buttonText, globalStyles.text]}>
+                Análise
+              </Text>
+            </Pressable>
+          </View>
+          <View style={styles.viewPagina}>
+            <Pressable
+              style={styles.buttonWrapper}
+              onPress={() => {
+                navigation.navigate("Relatorio");
+              }}
             >
-              <Path
-                d="M16.8846 95.4338C-1.49599 85.665 -5.25823 60.9379 9.38521 46.1445L34.8438 20.4253C36.8506 18.3979 38.3812 15.9495 39.3247 13.2574V13.2574C44.2839 -0.892831 62.8106 -4.17536 72.3295 7.4097L84.5358 22.2654C88.9527 27.641 91.3672 34.3828 91.3672 41.3402V42.9851C91.3672 48.2103 92.8969 53.3212 95.7676 57.6872L105.534 72.541C114.546 86.2478 103.011 104.14 86.8065 101.589V101.589C78.8025 100.329 70.8861 104.312 67.1277 111.49L66.8739 111.975C59.7559 125.57 40.5021 126.132 32.6028 112.976L28.023 105.349C25.7179 101.51 22.4108 98.3708 18.4567 96.2693L16.8846 95.4338Z"
-                fill="#ff80c3"
-              />
-            </Svg>
+              <Svg
+                width="90"
+                height="110"
+                viewBox="0 0 109 123"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <Path
+                  d="M16.8846 95.4338C-1.49599 85.665 -5.25823 60.9379 9.38521 46.1445L34.8438 20.4253C36.8506 18.3979 38.3812 15.9495 39.3247 13.2574V13.2574C44.2839 -0.892831 62.8106 -4.17536 72.3295 7.4097L84.5358 22.2654C88.9527 27.641 91.3672 34.3828 91.3672 41.3402V42.9851C91.3672 48.2103 92.8969 53.3212 95.7676 57.6872L105.534 72.541C114.546 86.2478 103.011 104.14 86.8065 101.589V101.589C78.8025 100.329 70.8861 104.312 67.1277 111.49L66.8739 111.975C59.7559 125.57 40.5021 126.132 32.6028 112.976L28.023 105.349C25.7179 101.51 22.4108 98.3708 18.4567 96.2693L16.8846 95.4338Z"
+                  fill="#ff80c3"
+                />
+              </Svg>
 
-            <View style={styles.buttonContent}>
-              <Image
-                source={require("../../assets/images/docum.png")}
-                style={styles.produtoImage}
-              />
-            </View>
-            <Text style={[styles.buttonText, globalStyles.text]}>
-              Relatório
-            </Text>
-          </Pressable>
-        </View>
+              <View style={styles.buttonContent}>
+                <Image
+                  source={require("../../assets/images/docum.png")}
+                  style={styles.produtoImage}
+                />
+              </View>
+              <Text style={[styles.buttonText, globalStyles.text]}>
+                Relatório
+              </Text>
+            </Pressable>
+          </View>
 
-        <View style={styles.viewPagina}>
-          <Pressable
-            style={styles.buttonWrapper}
-            onPress={() => {
-              navigation.navigate("Calendario");
-            }}
-          >
-            <Svg
-              width="90"
-              height="110"
-              viewBox="0 0 109 123"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+          <View style={styles.viewPagina}>
+            <Pressable
+              style={styles.buttonWrapper}
+              onPress={() => {
+                navigation.navigate("Calendario");
+              }}
             >
-              <Path
-                d="M16.8846 95.4338C-1.49599 85.665 -5.25823 60.9379 9.38521 46.1445L34.8438 20.4253C36.8506 18.3979 38.3812 15.9495 39.3247 13.2574V13.2574C44.2839 -0.892831 62.8106 -4.17536 72.3295 7.4097L84.5358 22.2654C88.9527 27.641 91.3672 34.3828 91.3672 41.3402V42.9851C91.3672 48.2103 92.8969 53.3212 95.7676 57.6872L105.534 72.541C114.546 86.2478 103.011 104.14 86.8065 101.589V101.589C78.8025 100.329 70.8861 104.312 67.1277 111.49L66.8739 111.975C59.7559 125.57 40.5021 126.132 32.6028 112.976L28.023 105.349C25.7179 101.51 22.4108 98.3708 18.4567 96.2693L16.8846 95.4338Z"
-                fill="#ff80c3"
-              />
-            </Svg>
+              <Svg
+                width="90"
+                height="110"
+                viewBox="0 0 109 123"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <Path
+                  d="M16.8846 95.4338C-1.49599 85.665 -5.25823 60.9379 9.38521 46.1445L34.8438 20.4253C36.8506 18.3979 38.3812 15.9495 39.3247 13.2574V13.2574C44.2839 -0.892831 62.8106 -4.17536 72.3295 7.4097L84.5358 22.2654C88.9527 27.641 91.3672 34.3828 91.3672 41.3402V42.9851C91.3672 48.2103 92.8969 53.3212 95.7676 57.6872L105.534 72.541C114.546 86.2478 103.011 104.14 86.8065 101.589V101.589C78.8025 100.329 70.8861 104.312 67.1277 111.49L66.8739 111.975C59.7559 125.57 40.5021 126.132 32.6028 112.976L28.023 105.349C25.7179 101.51 22.4108 98.3708 18.4567 96.2693L16.8846 95.4338Z"
+                  fill="#ff80c3"
+                />
+              </Svg>
 
-            <View style={styles.buttonContent}>
-              {/* <MaterialCommunityIcons
+              <View style={styles.buttonContent}>
+                {/* <MaterialCommunityIcons
                 name="clipboard-check-outline"
                 size={45}
                 color={"#2D2D2D"}
               /> */}
+                <Image
+                  source={require("../../assets/images/agenda.png")}
+                  style={styles.produtoImage}
+                />
+              </View>
+              <Text style={[styles.buttonText, globalStyles.text]}>
+                Calendário
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+
+        <View style={styles.jornada}>
+          <Text style={[globalStyles.mediumText, styles.textoJornada]}>
+            Verificação diária de Skincare
+          </Text>
+
+          <View>
+            <Pressable style={styles.feito}>
               <Image
-                source={require("../../assets/images/agenda.png")}
-                style={styles.produtoImage}
+                source={require("../../assets/images/relogio.png")}
+                style={styles.animationImage}
               />
+
+              <Text style={styles.textoRotinas}>Rotina Matinal</Text>
+            </Pressable>
+
+            <Pressable style={styles.feito2}>
+              <Image
+                source={require("../../assets/images/cloud.png")}
+                style={styles.animationImage}
+              />
+
+              <Text style={styles.textoRotinas}>Rotina Nortuna</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.wrapperContainer}>
+          <Swiper
+            style={styles.wrapper}
+            showsButtons={false}
+            autoplay={true}
+            autoplayTimeout={7}
+          >
+            <View style={styles.slide1}>
+              <Image
+                source={require("../../assets/images/sun.png")}
+                style={styles.image}
+              />
+              <Text style={styles.text}>Compartilhe o Skincare Routine</Text>
             </View>
-            <Text style={[styles.buttonText, globalStyles.text]}>
-              Calendário
-            </Text>
-          </Pressable>
+            <View style={styles.slide2}>
+              <Image
+                source={require("../../assets/images/sun.png")}
+                style={styles.image}
+              />
+              <Text style={styles.text}>Imagem 2</Text>
+            </View>
+            <View style={styles.slide3}>
+              <Image
+                source={require("../../assets/images/sun.png")}
+                style={styles.image}
+              />
+              <Text style={styles.text}>Imagem 3</Text>
+            </View>
+          </Swiper>
         </View>
-      </View>
-
-      <View style={styles.jornada}>
-        <Text style={[globalStyles.mediumText, styles.textoJornada]}>
-          Verificação diária de Skincare
-        </Text>
-
-        <View>
-          <Pressable style={styles.feito}>
-            <Image
-              source={require("../../assets/images/relogio.png")}
-              style={styles.animationImage}
-            />
-
-            <Text style={styles.textoRotinas}>Rotina Matinal</Text>
-          </Pressable>
-
-          <Pressable style={styles.feito2}>
-            <Image
-              source={require("../../assets/images/cloud.png")}
-              style={styles.animationImage}
-            />
-
-            <Text style={styles.textoRotinas}>Rotina Nortuna</Text>
-          </Pressable>
-        </View>
-      </View>
-      <View style={styles.wrapperContainer}>
-        <Swiper
-          style={styles.wrapper}
-          showsButtons={false}    
-          autoplay={true}        
-          autoplayTimeout={7}    
-          
-        >
-          <View style={styles.slide1}>
-            <Image
-              source={require("../../assets/images/sun.png")}
-              style={styles.image}
-            />
-            <Text style={styles.text}>Compartilhe o Skincare Routine</Text>
-          </View>
-          <View style={styles.slide2}>
-            <Image
-              source={require("../../assets/images/sun.png")}
-              style={styles.image}
-            />
-            <Text style={styles.text}>Imagem 2</Text>
-          </View>
-          <View style={styles.slide3}>
-            <Image
-              source={require("../../assets/images/sun.png")}
-              style={styles.image}
-            />
-            <Text style={styles.text}>Imagem 3</Text>
-          </View>
-        </Swiper>
-      </View>
-    </ScrollView>
+      </ScrollView>
     </>
   );
 }
@@ -239,12 +238,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffff",
     padding: 15,
     paddingBottom: 0,
-    marginBottom: 0
+    marginBottom: 0,
   },
   textoMenu: {
     fontSize: 15,
     color: "#a6a2a2",
-   
   },
   viewImagem: {
     width: "50%",
@@ -333,63 +331,65 @@ const styles = StyleSheet.create({
   },
   viewNotificacao: {
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   bellContainer: {
-    position: 'relative',
+    position: "relative",
     padding: 5,
   },
   badge: {
-    position: 'absolute',
+    position: "absolute",
     top: -5,
     right: -10,
-    backgroundColor: 'red', 
-    color: 'white', 
+    backgroundColor: "red",
+    color: "white",
   },
   wrapperContainer: {
-    width: '90%',
+    width: "90%",
     height: 150,
     margin: 20,
     borderRadius: 20,
-    overflow: 'hidden',
-    backgroundColor: 'transparent', 
-   
+    overflow: "hidden",
+    backgroundColor: "transparent",
   },
   swiper: {
-    borderRadius: 20, 
+    borderRadius: 20,
   },
   slide1: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: "red"
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
   },
   slide2: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: "red"
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
   },
   slide3: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: "red"
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
   },
   image: {
-    width: '50%',
-    height: '50%',
-    resizeMode: 'cover',
+    width: "50%",
+    height: "50%",
+    resizeMode: "cover",
   },
   text: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: 'bold',
-    position: 'absolute',
-    // bottom: 10,
-    // backgroundColor: 'rgba(0, 0, 255, 0.5)',
-    // paddingHorizontal: 10,
-    // paddingVertical: 5,
-    // borderRadius: 10,
+    fontWeight: "bold",
+    position: "absolute",
+  },
+  avatarContainer: {
+    width: 70,
+    height: 70,
+    borderRadius: 60,
+    borderWidth: 2,
+    borderColor: "#0076FD",
+    overflow: "hidden",
   },
 });
